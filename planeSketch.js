09bf3a54900d092f672ed117b6f9d2890ba03acb
kevzhong <shp2700@163.com>
2018-10-23 12:04:12 +0800

    2018-10-23-9

--- planeSketch.js
+++ planeSketch.js
@@ -338,13 +338,13 @@ function render() {
 
 
    // plane.rotation.y += 2-Math.random()*.02
-    document.getElementById('txt').innerHTML="x:"+Math.floor(accGravity.x)+"y:"+Math.floor(accGravity.y)+"id=8"
+    document.getElementById('txt').innerHTML="x:"+Math.floor(accGravity.x)+"y:"+Math.floor(accGravity.y)+"id=9"
     plane.position.x+=(accGravity.x/10)
 
     //plane.position.y+=(Math.random()*(0.05))
     // plane.position.y+=1+Math.random()*-2
 
-     plane.rotation.set(.2,-5.5+accGravity.x/10,0)
+     plane.rotation.set(.3,-4.5+accGravity.x/10,0)
 
     renderer.render(scene, camera);
 
