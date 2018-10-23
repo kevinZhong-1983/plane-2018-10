fa9f828c61eed5a5d13b39e9145e11420607782c
kevzhong <shp2700@163.com>
2018-10-23 11:31:01 +0800

    2018-10-23

--- planeSketch.js
+++ planeSketch.js
@@ -339,7 +339,7 @@ function render() {
 
    // plane.rotation.y += 2-Math.random()*.02
     document.getElementById('txt').innerHTML="x:"+Math.floor(accGravity.x)+"y:"+Math.floor(accGravity.y)
-    plane.position.x+=(accGravity/100)
+    plane.position.x+=(accGravity.x/100)
     // plane.position.y+=1+Math.random()*-2
 
     // plane.rotation.set(.2,-5.5,0)
